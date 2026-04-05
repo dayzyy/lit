a5d183241fab9b192b267861a55d05b510bb9f70
Luka Mania <luka.mania@leverx.com>
2026-04-05 16:16:36 +0400

    chore(tests): test repo creation

--- tests/unit/test_create_repo.py
+++ tests/unit/test_create_repo.py
@@ -1,35 +1,25 @@
 from pathlib import Path
 
 import pytest
-from _pytest.capture import CaptureFixture
-from _pytest.monkeypatch import MonkeyPatch
 
 from commands.init import create_repo
-from core.constants.directories import Directories
+from core.structure.exceptions import RepoExistsError
+from core.structure.structure import RepoStructure
 
 
-@pytest.fixture(autouse=True)
-def patch_cwd(tmp_path: Path, monkeypatch: MonkeyPatch) -> Path:
-    monkeypatch.setattr(Path, "cwd", lambda: tmp_path)
-    return tmp_path
+def test_create_repo_creates_expected_directories(tmp_path: Path):
+    create_repo(cwd=tmp_path)
 
-
-def test_create_repo_creates_expected_directories(patch_cwd: Path):
-    create_repo()
-
-    base = patch_cwd / Directories.DirectoryNames.BASE.value
+    base = tmp_path / RepoStructure.Directories.BASE.value
 
     assert base.exists()
 
-    for d in Directories.DirectoryNames:
-        if d == Directories.DirectoryNames.BASE:
-            continue
-        assert (base / d.value).exists()
+    for d in RepoStructure.Directories:
+        if d != RepoStructure.Directories.BASE:
+            assert (base / d.value).exists()
 
 
-def test_create_repo_notifies_when_repo_already_initialized(capsys: CaptureFixture[str]): # noqa: E501
-    create_repo()
-    create_repo()
-
-    captured = capsys.readouterr()
-    assert "Repository has already been initialized for this project!" in captured.out
+def test_create_repo_raises_when_repo_already_initialized(tmp_path: Path):
+    create_repo(cwd=tmp_path)
+    with pytest.raises(RepoExistsError):
+        create_repo(cwd=tmp_path)
