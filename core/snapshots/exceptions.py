84f7c60433edf95d780a94c07be3b66e84710d88
Luka Mania <luka.mania@leverx.com>
2026-06-08 20:01:54 +0400

    chore: add snapshot related exceptions

--- core/snapshots/exceptions.py
+++ core/snapshots/exceptions.py
@@ -1,7 +1,17 @@
 from core.utils.exceptions import BaseExceptionWithDefaultMessage as BaseExc
 
 INVALID_SNAPSHOT_SCHEMA_ERROR = "Invalid snapshot schema!"
+SNAPSHOT_FILE_NOT_FOUND_ERROR = "Snapshot file not found!"
+SNAPSHOT_FILE_ALREADY_EXISTS = "Snapshot file arleady exists!"
 
 
 class InvalidSnapshotSchemaError(BaseExc):
     message = INVALID_SNAPSHOT_SCHEMA_ERROR
+
+
+class SnapshotFileNotFoundError(BaseExc):
+    message = SNAPSHOT_FILE_NOT_FOUND_ERROR
+
+
+class SnapshotFileAlreadyExistsError(BaseExc):
+    message = SNAPSHOT_FILE_ALREADY_EXISTS
