662ed0ce332f58ea7f6be838a5ca4057f546d91a
Luka Mania <manialuka19@gmail.com>
2026-06-16 19:31:20 +0400

    tests: test snapshot file initialization in create_repo

--- tests/unit/test_create_repo.py
+++ tests/unit/test_create_repo.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import pytest
 
 from commands.init import create_repo
+from core.snapshots.reader import JSONSnapshotReader
+from core.snapshots.repo import SnapshotRepository
+from core.snapshots.writer import JSONSnapshotWriter
 from core.structure.exceptions import RepoExistsError
 from core.structure.structure import RepoStructure
 
@@ -23,3 +26,15 @@ def test_create_repo_raises_when_repo_already_initialized(tmp_path: Path):
     create_repo(cwd=tmp_path)
     with pytest.raises(RepoExistsError):
         create_repo(cwd=tmp_path)
+
+
+def test_create_repo_initializes_snapshot_file(tmp_path: Path):
+    lit_path = create_repo(
+        cwd=tmp_path, writer_cls=JSONSnapshotWriter, reader_cls=JSONSnapshotReader
+    )
+    snapshots_file_path = SnapshotRepository._get_file_path(lit_path)
+
+    reader = JSONSnapshotReader(snapshots_file_path)
+    snapshots = reader.read_snapshots()
+
+    assert snapshots == JSONSnapshotWriter.INITIAL_STRUCTURE
