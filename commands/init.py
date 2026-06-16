159922c81f9568216b75f9650210063f638f8730
Luka Mania <manialuka19@gmail.com>
2026-06-16 18:29:48 +0400

    refactor: initialize snpashot file in create_repo

--- commands/init.py
+++ commands/init.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from config import SNAPSHOT_WRITER_CLS
+from core.snapshots.repo import SnapshotRepository
 from core.structure.exceptions import RepoExistsError
 from core.structure.structure import RepoStructure
 
@@ -17,6 +19,9 @@ def create_repo(cwd: Path | None = None) -> Path:
         if dir is not RepoStructure.Directories.BASE:
             path = dir.get_path(lit_path)
             path.mkdir(parents=True)
+
+    snapshot_file_path = SnapshotRepository._get_file_path(lit_path)
+    SNAPSHOT_WRITER_CLS._initialize_file(snapshot_file_path)
 
     return lit_path
 
