4286bcb6219301c27ebba2241e36ed26401a6acb
Luka Mania <manialuka19@gmail.com>
2026-06-20 20:01:01 +0400

    feat: add latest and add methods to SnapshotRepo

--- core/snapshots/exceptions.py
+++ core/snapshots/exceptions.py
@@ -1,17 +1,22 @@
 from core.utils.exceptions import BaseExceptionWithDefaultMessage as BaseExc
 
-INVALID_SNAPSHOT_SCHEMA_ERROR = "Invalid snapshot schema!"
-SNAPSHOT_FILE_NOT_FOUND_ERROR = "Snapshot file not found!"
+INVALID_SNAPSHOT_SCHEMA = "Invalid snapshot schema!"
+SNAPSHOT_FILE_NOT_FOUND = "Snapshot file not found!"
 SNAPSHOT_FILE_ALREADY_EXISTS = "Snapshot file arleady exists!"
+SNAPSHOT_NOT_FOUND = "Snapshot not found!"
 
 
 class InvalidSnapshotSchemaError(BaseExc):
-    message = INVALID_SNAPSHOT_SCHEMA_ERROR
+    message = INVALID_SNAPSHOT_SCHEMA
 
 
 class SnapshotFileNotFoundError(BaseExc):
-    message = SNAPSHOT_FILE_NOT_FOUND_ERROR
+    message = SNAPSHOT_FILE_NOT_FOUND
 
 
 class SnapshotFileAlreadyExistsError(BaseExc):
     message = SNAPSHOT_FILE_ALREADY_EXISTS
+
+
+class SnapshotNotFoundError(BaseExc):
+    message = SNAPSHOT_FILE_NOT_FOUND
