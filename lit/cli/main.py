47d8f554774aff9a28bfa35bfa2a6509414d8391
Luka Mania <manialuka19@gmail.com>
2026-07-15 21:47:24 +0400

    chore: handle expected CLI errors with user-friendly messages

--- lit/cli/main.py
+++ lit/cli/main.py
@@ -1,17 +1,25 @@
+import sys
 from pathlib import Path
 
 from lit.cli.commands import LitCommand
 from lit.cli.parser import create_parser
+from lit.core.utils.exceptions import BaseExceptionWithDefaultMessage
 
 
 def main():
-    cwd = Path.cwd()
-    parser = create_parser()
-    args = parser.parse_args()
-    command: LitCommand = args.command_cls.from_args(args=args, cwd=cwd)
+    try:
+        cwd = Path.cwd()
+        parser = create_parser()
+        args = parser.parse_args()
+        command: LitCommand = args.command_cls.from_args(args=args, cwd=cwd)
 
-    command.run()
+        command.run()
+        return 0
+
+    except BaseExceptionWithDefaultMessage as exc:
+        print(exc, file=sys.stderr)
+        return 1
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
